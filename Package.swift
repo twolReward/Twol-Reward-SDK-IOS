fb1a1c262b947e21f91ce44b49585b3963c715d8
Ho Jin Joo <jooho0517@gmail.com>
2025-10-17 10:24:57 +0900

    Release v0.1.5

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TwolRewardSDK",
-            url: "https://github.com/twolReward/Twol-Reward-SDK-IOS/releases/download/0.1.4/TwolRewardSDK.xcframework.zip",
-            checksum: "7bbabb19455033773769df1f043da49770892bf6872b30470571cc7915d9b4ad"
+            url: "https://github.com/twolReward/Twol-Reward-SDK-IOS/releases/download/0.1.5/TwolRewardSDK.xcframework.zip",
+            checksum: "ad44a0e03f7a8451da5abcdf7b5e071533406cd44b1c63a634f9bab63bd2da48"
         )
     ]
 )
